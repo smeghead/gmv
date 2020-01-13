5d018381ac5bbb339fd7efe50e7a0fdba281573a
smeghead <smeghead7@gmail.com>
2020-01-13 13:07:43 +0900

    add test.

---
+++ src/gmv/go.mod
@@ -0,0 +1,5 @@
+module gmv
+
+go 1.13
+
+require github.com/spiegel-im-spiegel/file v0.1.1
--- src/gmv/main_test.go
+++ src/gmv/main_test.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"testing"
+	"reflect"
 )
 
-func Test_func(t *testing.T) {
-	var ret = fun()
+func Test_parse_no_match(t *testing.T) {
+	options := new(Option)
+	ret, err := parse(options, "testdata/case1/*.c", "testdata/case/*.x")
+	if err != nil {
+		t.Errorf("err: %v", err)
+	}
 	actual := ret
-	expected := "aaa"
-	if actual != expected {
+	expected := []Param{}
+	if len(actual) != len(expected) {
+		t.Errorf("got: %v\nwant: %v", len(actual), len(expected))
+	}
+//	if reflect.DeepEqual(expected, actual) {
+//		t.Errorf("got: %v\nwant: %v", actual, expected)
+//	}
+}
+func Test_parse_simple_match(t *testing.T) {
+	options := new(Option)
+	actual, err := parse(options, "testdata/case1/*", "testdata/case1/*.x")
+	if err != nil {
+		t.Errorf("err: %v", err)
+	}
+	expected := []Param{
+		{Src: "testdata/case1/01.txt", Dest: "testdata/case1/01.txt.x"},
+		{Src: "testdata/case1/02.txt", Dest: "testdata/case1/02.txt.x"},
+		{Src: "testdata/case1/03.txt", Dest: "testdata/case1/03.txt.x"},
+	}
+	if len(actual) != len(expected) {
+		t.Errorf("got: %v\nwant: %v", len(actual), len(expected))
+	}
+	if reflect.DeepEqual(expected, actual) {
 		t.Errorf("got: %v\nwant: %v", actual, expected)
 	}
 }
